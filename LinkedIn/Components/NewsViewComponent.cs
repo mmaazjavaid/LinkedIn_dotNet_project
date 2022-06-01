02b9bc9edcf3f16d444455e95285352452535073
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-01 10:43:09 +0500

    View Component added with static data

---
+++ LinkedIn/Components/NewsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace LinkedIn.Components
+{
+    public class NewsViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
