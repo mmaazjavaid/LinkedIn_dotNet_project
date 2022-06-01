34c39e48c828867243cfcc0b65358b92868f5f0e
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-01 13:12:48 +0500

    Jobs View Component added

---
+++ LinkedIn/Components/JobsViewComponent.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LinkedIn.Models;
+namespace LinkedIn.Components
+{
+    public class JobsViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Jobs> jobs = new List<Jobs>();
+            jobs.Add(new Jobs
+            {
+                CompanyLogo = "/images/tesla.jpg",
+                CompanyName = "Tesla",
+                Title = "On site Engineer",
+                EmployementType = "full-time",
+                JobType = "Hybrid",
+                Location = "lahore ,Pakistan"
+            });
+            jobs.Add(new Jobs
+            {
+                CompanyLogo = "/images/microsoft.jpg",
+                CompanyName = "Microsoft",
+                Title = "Software Engineer",
+                EmployementType = "full-time",
+                JobType = "Remote",
+                Location = "lahore ,Pakistan"
+            });
+            jobs.Add(new Jobs
+            {
+                CompanyLogo = "/images/twitter.png",
+                CompanyName = "Twitter",
+                Title = "AI Engineer required",
+                EmployementType = "full-time",
+                JobType = "on-site",
+                Location = "lahore ,Pakistan"
+            });
+            jobs.Add(new Jobs
+            {
+                CompanyLogo = "/images/google.png",
+                CompanyName = "Google",
+                Title = "Robotics Engineer urgent hiring",
+                EmployementType = "full-time",
+                JobType = "Hybrid",
+                Location = "lahore ,Pakistan"
+            });
+            return View("Default", jobs);
+        }
+    }
+}
---
+++ LinkedIn/Models/Jobs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkedIn.Models
+{
+    public class Jobs
+    {
+        public string Title { get; set; }
+        public string CompanyLogo { get; set; }
+        public string CompanyName { get; set; }
+        public string Location { get; set; }
+        public string JobType { get; set; }
+        public string EmployementType { get; set; }
+
+    }
+}
