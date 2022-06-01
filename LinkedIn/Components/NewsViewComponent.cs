877d5269699df5254e226d8f2d029d2cf9614728
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-06-01 12:42:40 +0500

    dynamic news added

--- LinkedIn/Components/NewsViewComponent.cs
+++ LinkedIn/Components/NewsViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using LinkedIn.Models;
 namespace LinkedIn.Components
 {
     public class NewsViewComponent : ViewComponent
@@ -6,7 +7,20 @@ namespace LinkedIn.Components
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            List<News> news = new List<News>();
+            News n = new News{ Name="Maaz Javaid",Likes=30,PersonImage= "/images/person.jpg", NewsImage="twitter.png"};
+            news.Add(n);
+            News n1 = new News { Name = "Usama Ijaz", Likes = 30, PersonImage = "/images/person.jpg", NewsImage = "twitter.png" };
+            news.Add(n1);
+            News n2 = new News { Name = "Faizan Ijaz", Likes = 50, PersonImage = "/images/person1.jpg", NewsImage = "twitter.png" };
+            news.Add(n2);
+            News n3 = new News { Name = "Matee ur rehman", Likes = 60, PersonImage = "/images/person2.jpg", NewsImage = "twitter.png" };
+            news.Add(n3);
+            News n4 = new News { Name = "Wahabi", Likes = 30, PersonImage = "/images/person3.jpg", NewsImage = "twitter.png" };
+            news.Add(n4);
+            News n5 = new News { Name = "Umer Waqar", Likes = 37, PersonImage = "/images/person4.jpg", NewsImage = "twitter.png" };
+            news.Add(n5);
+            return View("Default",news);
         }
     }
 }
---
+++ LinkedIn/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkedIn.Models
+{
+    public class News
+    {
+        public string Name { get; set; }
+        public int Likes { get; set; }
+        public string PersonImage { get; set; }
+        public string NewsImage { get; set; }
+
+    }
+}
